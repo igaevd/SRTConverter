fa1ff2b7960d48699f56659e1e773517972e4cef
igaevd <igaevd@gmail.com>
2024-04-23 17:48:44 -0400

    pdf changes

--- src/main/java/com/igayev/srtconverter/processor/Pdf.java
+++ src/main/java/com/igayev/srtconverter/processor/Pdf.java
@@ -1,10 +1,15 @@
 package com.igayev.srtconverter.processor;
 
+import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.kernel.colors.ColorConstants;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.Style;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Text;
 
 
 import java.io.*;
@@ -18,7 +23,13 @@ public class Pdf {
         PdfDocument pdf = new PdfDocument(writer);
         Document document = new Document(pdf, PageSize.A4);
         document.setMargins(10, 20, 10, 20);
-        Paragraph paragraph = new Paragraph(text).setFontSize(18);
+
+        Style style = new Style()
+                .setFont(PdfFontFactory.createFont(StandardFonts.SYMBOL))
+                .setFontSize(18);
+
+        Paragraph paragraph = new Paragraph(text)
+                .add(new Text(text).addStyle(style));
         document.add(paragraph);
         document.close();
     }
